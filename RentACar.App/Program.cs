ee0e7094015bac1cc3a633d73e69e6c378707fe4
Suat Alikoch <suat.alikoch@gmail.com>
2024-04-09 18:15:41 +0300

    + Changed user register requirements

--- RentACar.App/Program.cs
+++ RentACar.App/Program.cs
@@ -20,6 +20,16 @@ namespace RentACar.App
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredUniqueChars = 0;
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
