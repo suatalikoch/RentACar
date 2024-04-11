3de5a7ac4a19e0ff4db7a961c7205c8815079331
Suat Alikoch <suat.alikoch@gmail.com>
2024-04-11 17:48:12 +0300

    - Removed profile

--- RentACar.App/obj/Debug/net6.0/RentACar.App.AssemblyInfo.cs
+++ RentACar.App/obj/Debug/net6.0/RentACar.App.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49e2c10687e36421c85d16ed71b153c194633fcc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3db67ec53a2597bf3625418f1665678eda00acd")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
