03b6bbe91357b51afc8925371c26ae3f01c0d398
Suat Alikoch <suat.alikoch@gmail.com>
2024-04-11 16:34:20 +0300

    + Fixed bs toggle

--- RentACar.App/obj/Debug/net6.0/RentACar.App.AssemblyInfo.cs
+++ RentACar.App/obj/Debug/net6.0/RentACar.App.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e88e3d09dfd1d8e31a4f2fe81f33a2922822e5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+696c2b4ded5734c456459c594e77ebdc6c6dbfeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
