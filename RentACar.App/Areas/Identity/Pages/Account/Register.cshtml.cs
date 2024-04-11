a764c7ace34c5134fc7d534e00ebb619c4902710
Anton Dimitrov <antonjudo680@gmail.com>
2024-04-11 16:26:15 +0300

    + fixed username data input + fixed dropdown menu cars

--- RentACar.App/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RentACar.App/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -160,7 +160,7 @@ namespace RentACar.App.Areas.Identity.Pages.Account
                     PhoneNumber = Input.PhoneNumber
                 };
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
