b730132ef2afaf8e5cef260abb658319c6d867df
Suat Alikoch <suat.alikoch@gmail.com>
2024-04-12 17:09:13 +0300

    Revert "+ added seed data"
    This reverts commit b15db3d77d96415b6611845999182854fa813742.

--- RentACar.App/obj/Debug/net6.0/RentACar.App.AssemblyInfo.cs
+++ RentACar.App/obj/Debug/net6.0/RentACar.App.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cbd5a987824500bacaa6996755efb34199866a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
