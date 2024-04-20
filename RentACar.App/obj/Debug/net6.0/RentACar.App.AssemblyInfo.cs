8d468dd631194fc369a255727c3138eb6d8a4d7d
Anton Dimitrov <antonjudo680@gmail.com>
2024-04-20 23:53:04 +0300

    + updated user create/edit view model

--- RentACar.App/obj/Debug/net6.0/RentACar.App.AssemblyInfo.cs
+++ RentACar.App/obj/Debug/net6.0/RentACar.App.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4d81a10d5f27566c9138dbc757e653d9b978261")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentACar.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
